99f30e1202e39d272a47bb80f86aba8bc9dce915
Ke Li <like@umich.edu>
2013-06-10 09:49:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiscussGen::Application.config.secret_token = 'b8e495ca7260aaf9aee127916683f2f5fa20f874dfaa0e38219fb5c02e759bd826aa872ad1b6fa31a52265ed8eb25070045da6a90a9e00d2aa195871fe496386'
