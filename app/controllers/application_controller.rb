c6dbc3dd6e37a60c9d217503b6540c7f57038389
Ke Li <like@umich.edu>
2013-06-14 15:29:37 -0400

    Add friendly redirect in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  after_filter :store_location
+
+  def store_location
+    path = request.fullpath
+    unless path =~ /\/users/ || path =~ /\/memberships/
+      session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+    end
+  end
+
+  def redirect_back_or(default)
+    redirect_to session[:previous_url] || default
+    session.delete(:previous_url)
+  end
+
   def authenticate_privacy(authenticatable)
     unless authenticatable.visible_to?(current_user)
       flash[:error] = "This community is private"
       community = (authenticatable.class == Community) ? authenticatable : authenticatable.community
-      redirect_to new_community_membership_path(community)
+      redirect_back_or new_community_membership_path(community)
     end
   end
 
@@ -13,7 +27,7 @@ class ApplicationController < ActionController::Base
     community = (authenticatable.class == Community) ? authenticatable : authenticatable.community
     unless community.has_admin?(current_user)
       flash[:error] = "Insufficient privileges"
-      redirect_to root_url
+      redirect_back_or root_url
     end
   end
 end
