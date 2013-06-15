a750010e7aadb911def69a71aa4c0994f2fd1239
Ke Li <like@umich.edu>
2013-06-15 14:33:34 -0400

    Fix redirect for private community

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
     unless authenticatable.visible_to?(current_user)
       flash[:error] = "This community is private"
       community = (authenticatable.class == Community) ? authenticatable : authenticatable.community
-      redirect_back_or new_community_membership_path(community)
+      redirect_to new_community_membership_path(community)
     end
   end
 
