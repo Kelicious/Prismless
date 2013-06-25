30973d2355b6ebb62274508bc93d0f7266b0d764
Ke Li <like@umich.edu>
2013-06-25 18:01:26 -0400

    Change name to Unprismatic

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-OurCommunity::Application.configure do
+Unprismatic::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-OurCommunity::Application.configure do
+Unprismatic::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
