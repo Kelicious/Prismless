bf19c09df550cb851565e5058b2c1cca5e2f9f46
Ke Li <like@umich.edu>
2013-06-25 20:39:12 -0400

    Add titles for pages and title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Unprismatic"
+    if page_title && page_title.length > 0
+      "#{base_title} | #{page_title}"
+    else
+      base_title
+    end
+  end
 end
